bc008ce1c2c3ba5038fe263510f9003234a84bc2
Jason Williams <jason@williamstx.com>
2012-04-26 10:57:50 -0500

    commented the recursive checkbox algorithm

--- FolderTreeControl/Model/FolderTreeSelection.cs
+++ FolderTreeControl/Model/FolderTreeSelection.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GeekJ.FolderTreeControl.Model
 {
-    public class FolderTreeSelection
+    public class FolderTreeSelection : ViewModelBase
     {
         private List<Item> items = new List<Item>();
 
@@ -15,45 +15,55 @@ namespace GeekJ.FolderTreeControl.Model
         {
             if (folderTreeItem.SelectionItem == null && include)
             {
+                // node is checked for the first time, add a selection item
                 folderTreeItem.SelectionItem = new Item() { TreeItem = folderTreeItem, Include = include };
                 items.Add(folderTreeItem.SelectionItem);
             }
             else if (folderTreeItem.SelectionItem != null && folderTreeItem.SelectionItem.Include != include)
             {
+                // node with a current state is checked/unchecked
                 if (!include && (folderTreeItem.Parent == null || folderTreeItem.Parent.SelectionItem == null))
                 {
+                    // special behavior if root node is unchecked
                     items.RemoveAll(x => x.TreeItem.Equals(folderTreeItem));
                     folderTreeItem.SelectionItem = null;
                 }
                 else
                 {
+                    // determine if this node itself was specifically checked/unchecked
                     var match = items.Where(x => x.TreeItem.Equals(folderTreeItem)).SingleOrDefault();
                     if (match != null)
                     {
+                        // if so, adjust it's state
                         match.Include = include;
                     }
                     else
                     {
+                        // if not, add a new selection item
                         folderTreeItem.SelectionItem = new Item() { TreeItem = folderTreeItem, Include = include };
                         items.Add(folderTreeItem.SelectionItem);
                     }
                 }
             }
 
+            // process children nodes if any are loaded
             if (folderTreeItem.FoldersLoaded)
             {
                 foreach (var childItem in folderTreeItem.Folders)
                 {
+                    // all children node states should be wiped out and given a reference to this nodes state
                     items.RemoveAll(x => x.TreeItem.Equals(childItem));
                     childItem.SelectionItem = folderTreeItem.SelectionItem;
                     childItem.IsChecked = include;
+
+                    // recurse to go as many levels deep as we can
+                    // recursive calls will skip the code above because the selection item is already set here
                     Add(childItem, include);
                 }
             }
 
+            // give the parent nodes either checked or indeterminate states
             ProcessParents(folderTreeItem);
-
-            Console.WriteLine(items.Count());
         }
 
         private void ProcessParents(FolderTreeItem folderTreeItem)
@@ -62,9 +72,13 @@ namespace GeekJ.FolderTreeControl.Model
                 return;
             bool indeterminate = false;
             bool? current = folderTreeItem.IsChecked;
+
+            // walk up the tree
             do
             {
                 folderTreeItem = folderTreeItem.Parent;
+
+                // if any of the other children have a different checkbox state, set to indeterminate
                 if (indeterminate || folderTreeItem.Folders.Any(x => x.IsChecked != current))
                 {
                     folderTreeItem.IsChecked = null;
